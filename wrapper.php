a6f68586c199fcd14e2a1c5277bd5532bc8ccd82
Anne Dorko <anne@withoutboxes.com>
2017-02-16 09:53:52 +0100

    Added viewport for responsive design by default

--- wrapper.php
+++ wrapper.php
@@ -2,6 +2,7 @@
 <html <?php language_attributes(); ?>>
 <head>
   <meta charset="<?php bloginfo( 'charset' ); ?>">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
   <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
   <!--[if lt IE 9]>
   <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
