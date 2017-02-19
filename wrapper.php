026306e717cb4189ba116d5e2a823a17282b11f8
Anne Dorko <anne@withoutboxes.com>
2017-02-19 22:09:45 +0100

    Linted wrapper.php

--- wrapper.php
+++ wrapper.php
@@ -1,41 +1,41 @@
 <!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
-  <meta charset="<?php bloginfo( 'charset' ); ?>">
-  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-  <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
-  <!--[if lt IE 9]>
-  <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-  <![endif]-->
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
+	<!--[if lt IE 9]>
+	<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
 	<?php if ( is_singular() ) { wp_enqueue_script( 'comment-reply' ); } ?>
 	<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
 <header role="banner">
-  <div class="container">
-    <a href="<?php echo esc_url( home_url() )  ?>" class="logo">
-      <!-- Tip: SVG logo goes well here! -->
+	<div class="container">
+	<a href="<?php echo esc_url( home_url() )  ?>" class="logo">
+	  <!-- Tip: SVG logo goes well here! -->
 		<?php bloginfo( 'name' ); ?>
-    </a>
-    <!--
-    Tip: Uncomment this for mobile menus
-    <a class="navigation-menu-button" id="js-mobile-menu"><i class="fa fa-bars"></i></a>
-    -->
-    <nav role="navigation">
+	</a>
+	<!--
+	Tip: Uncomment this for mobile menus
+	<a class="navigation-menu-button" id="js-mobile-menu"><i class="fa fa-bars"></i></a>
+	-->
+	<nav role="navigation">
 		<?php wp_nav_menu( 'primary_menu' ); ?>
-    </nav>
-  </div>
+	</nav>
+	</div>
 </header>
 <main id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-    <div class="container">
-        <?php /* Main content goes here! */ ?>
-        <?php include twist_template_path(); ?>
-    </div>
+	<div class="container">
+		<?php /* Main content goes here! */ ?>
+		<?php include twist_template_path(); ?>
+	</div>
 </main>
 <footer>
-  <div class="container">
-    <p>Twist: a Starter Theme is built by <a href="http://www.annedorko.com/">Anne Dorko</a></p>
-  </div>
+	<div class="container">
+	<p>Twist: a Starter Theme is built by <a href="http://www.annedorko.com/">Anne Dorko</a></p>
+	</div>
 </footer>
 </body>
 <?php wp_footer(); ?>
